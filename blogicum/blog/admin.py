3dd18abbf8c4626925084cbff8e274e8b2351b55
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-05-08 15:32:04 +0500

    admin zone new funcs

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,8 +1,74 @@
 from django.contrib import admin
 
-from .models import Category, Location, Post
+from blog.models import Category, Location, Post
 
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'slug',
+    )
+    list_editable = (
+        'description',
+        'slug',
+    )
+    search_fields = (
+        'title',
+        'description',
+        'slug',
+    )
+    list_filter = ('title',)
+    list_display_links = ('title',)
+    ordering = ['slug']
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'is_published'
+    )
+    list_editable = (
+        'name',
+        'is_published',
+    )
+    search_fields = (
+        'id',
+        'name',
+    )
+    list_display_links = ('id',)
+    ordering = ['id']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'category',
+        'location',
+        'is_published',
+    )
+    list_editable = (
+        'author',
+        'category',
+        'location',
+        'is_published'
+    )
+    search_fields = (
+        'title',
+        'text',
+    )
+    list_filter = (
+        'author',
+        'category',
+        'is_published'
+    )
+    list_display_links = ('title',)
+    ordering = ['author__first_name']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Post, PostAdmin)
